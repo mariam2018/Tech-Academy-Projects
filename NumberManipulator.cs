bff52ad5fd428049a7c2b20a89879b1171f0b69d
mariam2018 <35977843+mariam2018@users.noreply.github.com>
2018-04-15 12:33:48 -0700

    Add files via upload

---
+++ NumberManipulator.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication10
+{
+    class NumberManipulator
+    {
+        public int FindMax(int num1, int num2)
+        {
+            /* local variable declaration */
+            int result;
+
+            if (num1 > num2)
+                result = num1;
+            else
+                result = num2;
+
+            return result;
+
+        }
+    }
+}
